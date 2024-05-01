de2601dcfa6c9a1ca9a5bc907088ede096668165
abulkais <abulkaish827@gmail.com>
2024-05-01 15:46:06 +0530

    Your descriptive commit message here

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import MyPDF from './ReactJSDeveloper.pdf';
+
 const About = () => {
+   
+
     return (
         <>
             <section className='about-sec' id='about'>
@@ -12,23 +15,24 @@ const About = () => {
 
                         </div>
                         <div className='col-md-6'>
-                            <div className=''>
-                                <h3>About Me</h3>
-                                <h5> Java FullStack Developer</h5>
-                                <p>I am a skilled Web Developer with a passion for creating dynamic and user-friendly websites. Proficient in <mark><b>HTML, CSS, JavaScript, Bootstrap and ReactJS</b></mark>, I craft visually appealing and responsive web pages. My expertise extends to backend development with <mark><b>Java, Spring Boot and SQL(MySql)</b></mark> enabling robust database management and server-side functionality. Additionally, I leverage React for building interactive and seamless user interfaces. My proficiency in Git ensures efficient version control and collaborative development. With a strong commitment to staying updated with the latest industry trends, I strive to deliver exceptional digital experiences.</p>
-                                <hr />
-                                <ul class="list-inline about-info">
-                                  
-                                    <li> <span>Email :</span> <p><a href="mailto:sadiq@example.com">abulkaish827@gmail.com</a></p> </li>
-                                    <li> <span>Number :</span> <p> <a href="tel:+91 8271057255">+91 8271-057-255</a> </p> </li>
-                                    <li> <span>Designation :</span> <p> Java Developer {'</>'}</p> </li>
-                                    <li><span>Address : </span><p>New Ashok Nagar, Delhi 96</p></li>
-                                    <li style={{width:'100%'}}><span>Hobbies : </span><p><b>1.</b> Programming <b>2.</b> Reading Books</p>  </li>
-                                </ul>
-                              
-                                <Link  className='banner-btn btn-color' to="webdeveloepr.pdf" target="_blank" download>Resume</Link>
 
-                            </div>
+                            <h3>About Me</h3>
+                            <h5> Frontend Developer</h5>
+                            <p>I am a skilled Web Developer with a passion for creating dynamic and user-friendly websites. Proficient in <mark><b>HTML, CSS, JavaScript, Bootstrap, ReactJS and Redux</b></mark>, I craft visually appealing and responsive web pages. My expertise extends to backend development with <mark><b>Java, and SQL(MySql)</b></mark> enabling robust database management and server-side functionality. Additionally, I leverage React for building interactive and seamless user interfaces. My proficiency in Git ensures efficient version control and collaborative development. With a strong commitment to staying updated with the latest industry trends, I strive to deliver exceptional digital experiences.</p>
+                            <hr />
+                            <ul class="list-inline about-info">
+
+                                <li> <span>Email :</span> <p><a href="mailto:sadiq@example.com">abulkaish827@gmail.com</a></p> </li>
+                                <li> <span>Number :</span> <p> <a href="tel:+91 8271057255">+91 8271-057-255</a> </p> </li>
+                                <li> <span>Designation :</span> <p> ReactJS Developer {'</>'}</p> </li>
+                                <li><span>Address : </span><p>New Ashok Nagar, Delhi 96</p></li>
+                                <li style={{ width: '100%' }}><span>Hobbies : </span><p><b>1.</b> Programming <b>2.</b> Reading Books</p>  </li>
+                            </ul>
+
+                        
+                            <a href={MyPDF} download="resume.pdf" className='banner-btn btn-color' > Download Resume </a>
+
+
                         </div>
                     </div>
                 </div>
--- src/Components/Technologies.jsx
+++ src/Components/Technologies.jsx
@@ -5,74 +5,75 @@ const Technologies = () => {
         <>
             <section className='services-sec' id='technologies'>
                 <div className='container'>
-                    {/* <div style={{ height: '300px', overflowY: 'scroll', overflowX: 'auto' }}> */}
-                    <h2 className='text-center' style={{ fontWeight: 'bold' }}>Frontend Technologies</h2>
+                  
+                    <h2 className='text-center' style={{ fontWeight: 'bold' }}> Technologies</h2>
                     <div className='row' >
-                        <div className='col-md-2 mx-auto mt-5'>
+                        <div className='col-md-3 mx-auto mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://cdn-icons-png.flaticon.com/512/1216/1216733.png" alt="" />
+                                <img src="https://cdn-icons-png.flaticon.com/512/1216/1216733.png" alt="reactJS" />
                                 <h5>HTML 5</h5>
 
                             </div>
                         </div>
-                        <div className='col-md-2 mx-auto mt-5'>
+                        <div className='col-md-3 mx-auto mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png" alt="" />
+                                <img src="https://cdn4.iconfinder.com/data/icons/social-media-logos-6/512/121-css3-512.png" alt="reactJS" />
                                 <h5>CSS 3</h5>
 
                             </div>
                         </div>
-                        <div className='col-md-2 mx-auto mt-5'>
+                        <div className='col-md-3 mx-auto mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg" alt="" />
+                                <img src="https://icon-library.com/images/javascript-icon-png/javascript-icon-png-23.jpg" alt="reactJS" />
                                 <h5>JavaScript</h5>
 
                             </div>
                         </div>
 
-                        <div className='col-md-2 mx-auto mt-5'>
+                        <div className='col-md-3 mx-auto mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/bootstrap-5-logo-icon.png" alt="" />
+                                <img src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/bootstrap-5-logo-icon.png" alt="reactJS" />
                                 <h5>Bootstrap 5</h5>
 
                             </div>
                         </div>
-                        <div className='col-md-2 mx-auto mt-5'>
+                        <div className='col-md-3 mx-auto mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png" alt="" />
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1150px-React-icon.svg.png" alt="reactJS" />
                                 <h5>ReactJS</h5>
 
                             </div>
                         </div>
-                    </div>
-                    <h2 className='text-center mt-5' style={{ fontWeight: 'bold' }}>Backend Technologies & DataBase</h2>
-                    <div className='row' >
+
                         <div className='col-md-3 mx-auto mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://cdn.iconscout.com/icon/free/png-512/free-java-59-1174952.png?f=avif&w=256" alt="" />
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxlr-8KCmtLR5ilOzm866fjks_b17xwA2lhfviQ8vQTQ&s" alt="reactJS" />
+                                <h5>Redux</h5>
+
+                            </div>
+                        </div>
+                  
+                        <div className='col-md-3 mx-auto mt-5'>
+                            <div className='services_sec_inner'>
+                                <img src="https://cdn.iconscout.com/icon/free/png-512/free-java-59-1174952.png?f=avif&w=256" alt="reactJS" />
                                 <h5>Java </h5>
 
                             </div>
                         </div>
-                        <div className='col-md-3 mx-auto mt-5'>
-                            <div className='services_sec_inner'>
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwsq-7f5BWyog4cdeT1sQaYLVzhJ0o37Up8TjHvVU08WUgfyyMMRMHTVwJ5XReSjyhZa0&usqp=CAU" alt="" />
-                                <h5>Spring Boot</h5>
-                            </div>
-                        </div>
+                 
 
                         <div className='col-md-3  mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://upload.wikimedia.org/wikipedia/labs/8/8e/Mysql_logo.png" alt="" />
+                                <img src="https://upload.wikimedia.org/wikipedia/labs/8/8e/Mysql_logo.png" alt="reactJS" />
                                 <h5> MySql</h5>
 
                             </div>
                         </div>
 
 
-                        <div className='col-md-3  mt-5'>
+                        <div className='col-md-3  mt-5 mx-auto'>
                             <div className='services_sec_inner'>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="" />
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/2048px-Octicons-mark-github.svg.png" alt="reactJS" />
                                 <h5> GitHub</h5>
 
                             </div>
@@ -89,30 +90,30 @@ const Technologies = () => {
                     <div className='row mx-auto' >
                         <div className='col-md-3 mx-auto mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png" alt="" />
+                                <img src="https://yt3.googleusercontent.com/_q52i8bUAEvcb7JR4e-eNTv23y2A_wg5sCz0NC0GrGtcw1CRMWJSOPVHUDh_bngD0q4gMvVeoA=s900-c-k-c0x00ffffff-no-rj" alt="reactJS" />
                                 <h5>Visual Studio Code (VS Code) </h5>
 
                             </div>
                         </div>
                         <div className='col-md-3 mx-auto mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx_JmK4HGxY6MYO4qbXK0d_Ieyrr7VBJcBCofhR58O1pUXpm-XE6tijyFShq7hhZZo5p4&usqp=CAU" alt="" />
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQx_JmK4HGxY6MYO4qbXK0d_Ieyrr7VBJcBCofhR58O1pUXpm-XE6tijyFShq7hhZZo5p4&usqp=CAU" alt="reactJS" />
                                 <h5>Eclipse</h5>
                             </div>
                         </div>
 
                         <div className='col-md-3  mt-5'>
                             <div className='services_sec_inner'>
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/IntelliJ_IDEA_Icon.svg/2048px-IntelliJ_IDEA_Icon.svg.png" alt="" />
-                                <h5> IntelliJ IDEA </h5>
+                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzEGJtJLv792PynY3GJPByOQCvLKIFSgoOnDUPXTiwnQ&s" alt="reactJS" />
+                                <h5> Sublime text </h5>
 
                             </div>
                         </div>
 
                         <div className='col-md-3  mt-5'>
                             <div className='services_sec_inner'>
-                                <img style={{width:'70px'}} src="https://pbs.twimg.com/profile_images/1235954979783012354/t9lO8UAz_400x400.png" alt="" />
-                                <h5> STS </h5>
+                                <img style={{width:'70px'}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl7W8BlJ_tUCOAEj2IxYFBkrgfNhuILHNO8MBvylM_7g&s" alt="reactJS" />
+                                <h5> Atom </h5>
 
                             </div>
                         </div>
